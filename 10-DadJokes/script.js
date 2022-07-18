38ca8a40b5bc298588834a542859209226fecc80
0xlvl3 <cryptochoccy@gmail.com>
2022-07-18 15:52:22 +1000

    Progress

--- 10-DadJokes/script.js
+++ 10-DadJokes/script.js
@@ -8,16 +8,19 @@ btn.addEventListener("click", generateJoke);
 generateJoke();
 
 //Using an API
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com/", config)
-    .then((response) => response.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  const response = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await response.json();
+  jokeEl.innerHTML = data.joke;
+
+  // .then((response) => response.json())
+  // .then((data) => {
+  //   jokeEl.innerHTML = data.joke;
+  // });
 }
