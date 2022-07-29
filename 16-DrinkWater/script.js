80d5ee9394806ac14aa8e265d8ec5211746ba40d
0xlvl3 <cryptochoccy@gmail.com>
2022-07-29 19:00:35 +1000

    Progress

---
+++ 16-DrinkWater/script.js
@@ -0,0 +1,51 @@
+const smallCup = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+updateBigCup();
+
+smallCup.forEach((cup, i) => {
+  cup.addEventListener("click", () => highlightCups(i));
+});
+
+function highlightCups(i) {
+  if (
+    smallCup[i].classList.contains("full") &&
+    !smallCup[i].nextElementSibling.classList.contains("full")
+  ) {
+    i--;
+  }
+
+  smallCup.forEach((cup, i2) => {
+    if (i2 <= i) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCup.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 300}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
